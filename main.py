2bc60985a0c239bb59ab9fd1cefc7ffbeaf993f6
juan diego zipaquira <jann@LAPTOP-19TE6QG9.localdomain>
2022-09-28 16:56:03 -0500

    Velidations bodys

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from typing import Optional
 from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI()
 #Models
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+    
+    
 class Person(BaseModel):
     first_name: str
     last_name: str
@@ -50,8 +56,20 @@ def create_person(persona: Person = Body(...)) -> Person:
 #Validation: Query parameters.
 @app.get("/person/new")
 def show_person( 
-    name: Optional[str] =  Query(None,min_length=1,max_length=50),
-    age: Optional[int] = Query(0,gt=1,lt=50)
+    name: Optional[str] =  Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person name",
+        description="this parameter is optional, Name of individual" 
+        ),
+    age: Optional[int] = Query(
+        0,
+        gt=0,
+        lt=100,
+        title="Person age",
+        description="The age is optional, has to be less than 100"
+        )
     ):
     """_summary_
         This fuction use a decorator, Query with
@@ -66,4 +84,33 @@ def show_person(
     return {name : age}
 
 
+#Validacion: Path parameters.
+@app.get("/person/detail/{person_id}")
+def show_person_detail(
+    person_id: str = Path(
+        ...,
+        min_length=6,
+        title ="Number id of the person.",
+        description="This parameter is required,Identification of the person."
+        )
+    
+):
+    return {person_id : "It exists!"}
+
+#Validaciones: Request Body.
+
+@app.put("/person/{person_id}") 
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person ID",
+        description="This is the person ID",
+        gt = 0
+        ),
+    person: Person = Body(...),
+    location: Location = Body(...)   
+) -> Person:
+    result = dict(person)
+    result.update(dict(location))
+    return result
 
